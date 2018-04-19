b125063c68928a9cd3b31085928086057ba8daff
xucheng <xucheng@yme.me>
2018-04-19 14:39:39 +0800

    init ortools

---
+++ src/main/java/me/ymq/ortools/OrtoolsApplication.java
@@ -0,0 +1,12 @@
+package me.ymq.ortools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrtoolsApplication.class, args);
+	}
+}
