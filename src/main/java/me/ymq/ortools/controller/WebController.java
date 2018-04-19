e5a1277902a1cf8be14e1ed550e4dcd369bd2ea9
xucheng <xucheng@yme.me>
2018-04-19 15:51:04 +0800

    add samples

--- src/main/java/me/ymq/ortools/controller/WebController.java
+++ src/main/java/me/ymq/ortools/controller/WebController.java
@@ -1,5 +1,6 @@
 package me.ymq.ortools.controller;
 
+import me.ymq.ortools.sample.Sudoku;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,11 @@ public class WebController {
     @GetMapping("/hello.do")
     public String hello() {
         return "hello world!";
+    }
+
+    @GetMapping("/sudoku.do")
+    public String sudoku() {
+        Sudoku.solve();
+        return "sudoku!";
     }
 }
