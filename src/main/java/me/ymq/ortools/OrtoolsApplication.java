4a9da8d0aa0144fdf6314e12ed16106ba14676c2
xucheng <xucheng@yme.me>
2018-04-20 11:06:07 +0800

    revert so files

--- src/main/java/me/ymq/ortools/OrtoolsApplication.java
+++ src/main/java/me/ymq/ortools/OrtoolsApplication.java
@@ -9,9 +9,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class OrtoolsApplication extends SpringBootServletInitializer {
     static {
         NativeLibLoader.load("classpath:jniortools.dll", "jniortools.dll");
-        NativeLibLoader.load("classpath:libfap.so", "libfap.so");
-        NativeLibLoader.load("classpath:libdimacs.so", "libdimacs.so");
-        NativeLibLoader.load("classpath:libcvrptw_lib.so", "libcvrptw_lib.so");
         NativeLibLoader.load("classpath:libortools.so", "libortools.so");
         NativeLibLoader.load("classpath:libjniortools.so", "libjniortools.so");
     }
